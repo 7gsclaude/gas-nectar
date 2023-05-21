2b7c9dd305a28804fbcd1ffc620ca540cc10fdbc
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-05-21 14:08:17 -0500

    starting gas and nectar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/navbar.js'
+import Home from './components/home.js'
+import About from './components/about.js'
+import Cart from './components/cart.js'
+import Login from './components/login.js'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/cart" exact component={Cart} /> 
+           <Route path="/login" component={Login} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div className='navbar'>
+          <div className='links'>
+              <a href='/'>Home</a>
+              <a href='/about'>About</a>
+              <a href='/cart'>Cart</a>
+              <a href='/login'>Login</a>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar

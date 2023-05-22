f5a7c6a0339d25f3bc9c7da1d807b4c245c19927
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-05-22 18:02:31 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/navbar.js'
-import Home from './components/home.js'
-import About from './components/about.js'
-import Cart from './components/cart.js'
-import Login from './components/login.js'
+import Navbar from './components/Navbar.js'
+
+import Cart from './components/Cart.js'
+
 
 
 
@@ -16,10 +15,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
+   
           <Route path="/cart" exact component={Cart} /> 
-           <Route path="/login" component={Login} />
+         
           
         </Routes>
       </Router>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+  background-color: aqua;
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding: 1rem;
+`;
+
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+  padding: 0.5rem;
+`;
 
 const Navbar = () => {
   return (
-      <div className='navbar'>
-          <div className='links'>
-              <a href='/'>Home</a>
-              <a href='/about'>About</a>
-              <a href='/cart'>Cart</a>
-              <a href='/login'>Login</a>
-          </div>
-    </div>
-  )
-}
+    <NavbarContainer>
+      <LinksContainer>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/about">About</NavLink>
+        <NavLink href="/cart">Cart</NavLink>
+        <NavLink href="/login">Login</NavLink>
+      </LinksContainer>
+    </NavbarContainer>
+  );
+};
 
-export default Navbar
+export default Navbar;
